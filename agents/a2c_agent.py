0835ca0bda53108ade65bbcea39eda37544e3ffb
Francisco Ramos <jscriptcoder@gmail.com>
2019-09-29 14:33:06 +0200

    WIP: latest changes

--- agents/a2c_agent.py
+++ agents/a2c_agent.py
@@ -20,17 +20,25 @@ class Policy(nn.Module):
         
         self.critic = nn.Sequential(
             nn.Linear(input_size, hidden_size),
-            nn.ReLU(),
+            nn.BatchNorm1d(hidden_size),
+            nn.Tanh(),
+            nn.Dropout(.3),
             nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
-            nn.Linear(hidden_size, 1)
+            nn.BatchNorm1d(hidden_size),
+            nn.Tanh(),
+            nn.Dropout(.3),
+            nn.Linear(hidden_size, 1),
         )
         
         self.actor = nn.Sequential(
             nn.Linear(input_size, hidden_size),
-            nn.ReLU(),
+            nn.BatchNorm1d(hidden_size),
+            nn.Tanh(),
+            nn.Dropout(.3),
             nn.Linear(hidden_size, hidden_size),
-            nn.ReLU(),
+            nn.BatchNorm1d(hidden_size),
+            nn.Tanh(),
+            nn.Dropout(.3),
             nn.Linear(hidden_size, output_size),
             nn.Softmax(dim=1),
         )
@@ -46,8 +54,10 @@ class A2CAgent:
     def __init__(self, 
                  state_size, action_size,
                  hidden_size=64,
-                 optim=optim.Adam, 
-                 lr=1e-3):
+                 optim=optim.RMSprop, 
+                 lr=7e-4, 
+                 alpha=0.99,
+                 eps=1e-5):
         
         super(A2CAgent, self).__init__()
         
@@ -55,7 +65,10 @@ class A2CAgent:
                              action_size, 
                              hidden_size=hidden_size).to(device)
         
-        self.optim = optim(self.policy.parameters(), lr=lr)
+        self.optim = optim(self.policy.parameters(), 
+                           lr=lr, 
+                           alpha=alpha,
+                           eps=eps)
     
     def act(self, state):
         state = torch.FloatTensor(state).to(device)
@@ -67,84 +80,97 @@ class A2CAgent:
     def train(self, 
               envs, 
               n_episodes=2000,
-              max_t=20, 
+              max_t=1000, 
+              steps=5,
               gamma=.99, 
               coef_ent=.01, 
               coef_val=.25, 
-              print_every=100):
+              clip_val=.5,
+              print_every=100,
+              solved=1.0):
 
         num_envs = len(envs.ps)
         scores_deque = deque(maxlen=100)
         
         for i_episode in range(1, n_episodes+1):
             
-            log_probs = []
-            values = []
-            entropies = []
-            rewards = []
-            masks = []
             scores = 0
-            
             state = envs.reset()
             
             for t in range(max_t):
-                action, value, dist = self.act(state)
                 
-                log_probs.append(dist.log_prob(action).unsqueeze(-1))
-                values.append(value)
-                entropies.append(dist.entropy().unsqueeze(-1))
+                log_probs = []
+                values = []
+                entropies = []
+                rewards = []
+                masks = []
                 
-                next_state, reward, done, _ = envs.step(action.cpu().numpy())
+                # Collect n steps of data
+                for s in range(steps):
+                    action, value, dist = self.act(state)
+                    
+                    log_probs.append(dist.log_prob(action).unsqueeze(-1))
+                    values.append(value)
+                    entropies.append(dist.entropy().unsqueeze(-1))
+                    
+                    next_state, reward, done, _ = envs.step(action.cpu().numpy())
+                    
+                    scores += reward.mean()
+                    
+                    rewards.append(torch.FloatTensor(reward).unsqueeze(-1).to(device))
+                    masks.append(torch.FloatTensor(1 - done).unsqueeze(-1).to(device))
+                    
+                    state = next_state
+                    
+                    if done.any():
+                        break
+            
+                next_state = torch.FloatTensor(next_state).to(device)
+                _, next_value = self.policy(next_state)
                 
-                scores += reward.mean()
+                # return = R + yV(s)
+                returns = next_value.detach()
+                for i in reversed(range(len(rewards))):
+                    returns = rewards[i] + gamma * returns * masks[i]
                 
-                rewards.append(torch.FloatTensor(reward).unsqueeze(-1).to(device))
-                masks.append(torch.FloatTensor(1 - done).unsqueeze(-1).to(device))
+                log_probs = torch.cat(log_probs).view(num_envs, -1)
+                values = torch.cat(values).view(num_envs, -1)
+                entropies = torch.cat(entropies).view(num_envs, -1)
                 
-                state = next_state
+                # advantage = R + yV(s') - V(s)
+                advantage = returns - values.detach()
                 
+                # A2C losses
+                policy_loss = -(log_probs * advantage).mean()
+                value_loss = (returns - values).pow(2).mean()
+                policy_ent = entropies.mean()
+                
+                loss = policy_loss + (coef_val * value_loss) + (coef_ent * policy_ent)
+                
+                self.optim.zero_grad()
+                loss.backward()
+                
+                grad_norm = nn.utils.clip_grad_norm_(self.policy.parameters(), clip_val)
+                
+                self.optim.step()
+            
                 if done.any():
                     break
             
             scores_deque.append(scores)
-            
-            next_state = torch.FloatTensor(next_state).to(device)
-            _, next_value = self.policy(next_state)
-            
-            # return = R + yV(s)
-            returns = next_value.detach()
-            for i in reversed(range(len(rewards))):
-                returns = rewards[i] + gamma * returns * masks[i]
-            
-            log_probs = torch.cat(log_probs).view(num_envs, -1)
-            values = torch.cat(values).view(num_envs, -1)
-            entropies = torch.cat(entropies).view(num_envs, -1)
-            
-            # advantage = R + yV(s') - V(s)
-            advantage = returns - values.detach()
-            
-            actor_loss = -(log_probs * advantage).mean()
-            critic_loss = (returns - values).pow(2).mean()
-            entropy_loss = entropies.mean()
-            
-            loss = actor_loss + (coef_val * critic_loss) - (coef_ent * entropy_loss)
-            
-            self.optim.zero_grad()
-            loss.backward()
-            self.optim.step()
-            
             score_mean = np.mean(scores_deque)
             
             if i_episode % print_every == 0:
-                print('Episode {}\tAverage Score: {:.3f}\tActor loss: {:.3f}\tCritic loss: {:.3f}\tEntropy loss: {:.3f}\tTotal loss: {:.3f}'\
+                print('Episode {}\tAverage Score: {:.3f}\tPolicy loss: {:.3f}\tValue loss: {:.3f}\tEntropy: {:.3f}\tTotal loss: {:.3f}\tGrad norm: {:.3f}'\
                       .format(i_episode, 
                               score_mean, 
-                              actor_loss, 
-                              critic_loss, 
-                              entropy_loss,
+                              policy_loss, 
+                              value_loss, 
+                              policy_ent,
+                              grad_norm,
                               loss))
             
-            if score_mean >= 195.0:
+            if score_mean >= solved:
                 print('Environment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, score_mean))
                 break
         
--- main.py
+++ main.py
@@ -3,22 +3,21 @@ import rocket_lander_gym
 from utils import multi_env, run_env
 from agents.a2c_agent import A2CAgent
 
-num_envs = 1
-env_name = 'CartPole-v0' # RocketLander-v0 | MountainCar-v0 | CartPole-v0
+num_envs = 5
+env_name = 'RocketLander-v0' # RocketLander-v0 | MountainCar-v0 | CartPole-v0
 
 envs = multi_env(env_name, num_envs)
 
 state_size = envs.observation_space.shape[0]
 action_size = envs.action_space.n
 
-agent = A2CAgent(state_size, action_size, hidden_size=256, lr=1e-3)
+agent = A2CAgent(state_size, action_size, hidden_size=256)
 
-scores = agent.train(envs, 
-                     max_t=1000, 
-                     coef_val=0.25, 
-                     coef_ent=0.01)
+scores = agent.train(envs)
 
-torch.save(agent.policy.state_dict(), 'policy_weights.pth')
+#####################################################################
+
+torch.save(agent.policy.state_dict(), 'poli   cy_weights.pth')
 
 agent.policy.load_state_dict(torch.load('policy_weights.pth'))
 
@@ -33,7 +32,8 @@ import gym
 
 env = gym.make(env_name)
 #env = gym.wrappers.Monitor(env, "recording")
-#    
+
+agent.policy.eval()
 state = env.reset()
 env.render()
 
