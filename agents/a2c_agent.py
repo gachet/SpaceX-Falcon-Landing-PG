22169b0aa6b3494ff0fb8c92e4bf1a508ea7b8ce
Francisco Ramos <jscriptcoder@gmail.com>
2019-10-02 09:55:22 +0200

    Using two separate networks for policy and value

--- agents/a2c_agent.py
+++ agents/a2c_agent.py
@@ -1,6 +1,6 @@
-import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.distributions import Categorical
 
 from .device import device
@@ -21,25 +21,34 @@ class Policy(nn.Module):
         
         dims = (state_dim,) + hidden_units
         
-        self.feature_layers = nn.ModuleList(
-            [layer_init(nn.Linear(dim_in, dim_out)) \
-             for dim_in, dim_out \
-             in zip(dims[:-1], dims[1:])])
+        feature_layers = []
+#        batchnorm_layers = []
+        
+        for dim_in, dim_out in  zip(dims[:-1], dims[1:]):
+            feature_layers.append(layer_init(nn.Linear(dim_in, dim_out)))
+#            batchnorm_layers.append(nn.BatchNorm1d(dim_out))
+        
+        self.feature_layers = nn.ModuleList(feature_layers)
+#        self.batchnorm_layers = nn.ModuleList(batchnorm_layers)
     
         self.activ = activ
     
-        features_output = dims[-1]
+        features_out = dims[-1]
         
-        self.action_layer = layer_init(nn.Linear(features_output, action_dim))
-        self.value_layer = layer_init(nn.Linear(features_output, 1))
+        self.action_layer = layer_init(nn.Linear(features_out, action_dim))
+        self.value_layer = layer_init(nn.Linear(features_out, 1))
         
         self.to(device)
 
     def forward(self, states):
         x = torch.FloatTensor(states)
         
-        for layer in self.feature_layers:
-            x = self.activ(layer(x))
+        for linear in self.feature_layers:
+#        for linear, batch_norm in zip(self.feature_layers, self.batchnorm_layers):
+            x = linear(x)
+#            x = batch_norm(x)
+            x = self.activ(x)
+            x = F.dropout(x, 0.5)
     
         logits = self.action_layer(x)
         values = self.value_layer(x)
@@ -108,6 +117,7 @@ class Critic(nn.Module):
         
         for layer in self.layers:
             x = self.activ(layer(x))
+            x = F.dropout(x, 0.3)
     
         return x
 
@@ -117,24 +127,26 @@ class A2CAgent:
         super(A2CAgent, self).__init__()
         self.config = config
         
-        self.policy = Policy(config.state_dim, 
-                             config.action_dim, 
-                             config.hidden_units, 
-                             config.activ)
+#        self.policy = Policy(config.state_dim, 
+#                             config.action_dim, 
+#                             config.hidden_units, 
+#                             config.activ)
         
-#        self.policy = Actor(config.state_dim, 
-#                            config.action_dim, 
-#                            config.hidden_units, 
-#                            config.activ)
-#        
-#        self.value = Critic(config.state_dim, 
-#                            config.hidden_units, 
-#                            config.activ)
+        self.policy = Actor(config.state_dim, 
+                            config.action_dim, 
+                            config.hidden_actor, 
+                            config.activ_actor)
         
-        self.optim = config.optim(self.policy.parameters(), lr=config.lr)
+        self.value = Critic(config.state_dim, 
+                            config.hidden_critic, 
+                            config.activ_critic)
         
-#        self.optim_policy = config.optim(self.policy.parameters(), lr=config.lr)
-#        self.optim_value = config.optim(self.value.parameters(), lr=config.lr)
+#        self.optim = config.optim(self.policy.parameters(), lr=config.lr)
+        
+        self.optim_policy = config.optim_actor(self.policy.parameters(), 
+                                               lr=config.lr_actor)
+        self.optim_value = config.optim_critic(self.value.parameters(), 
+                                               lr=config.lr_critic)
         
         self.reset()
     
@@ -144,11 +156,9 @@ class A2CAgent:
     
     def act(self, state):
         self.policy.eval()
-        
         with torch.no_grad():
-            action, _, _, _ = self.policy(state)
-#            action, _, _ = self.policy(state)
-        
+#            action, _, _, _ = self.policy(state)
+            action, _, _ = self.policy(state)
         self.policy.train()
         
         return action
@@ -156,7 +166,7 @@ class A2CAgent:
     def step(self):
         config = self.config
         envs = config.envs
-        num_envs = len(envs.ps)
+        num_envs = envs.nenvs
         state = self.state
         
         actions = []
@@ -166,11 +176,11 @@ class A2CAgent:
         rewards = []
         masks = []
                 
-        for _ in range(config.rollout):
-            action, log_prob, entropy, value = self.policy(state)
+        for _ in range(config.rollouts):
+#            action, log_prob, entropy, value = self.policy(state)
             
-#            action, log_prob, entropy = self.policy(state)
-#            value = self.value(state)
+            action, log_prob, entropy = self.policy(state)
+            value = self.value(state)
             
             next_state, reward, done, _ = envs.step(action.cpu().numpy())
             
@@ -190,57 +200,58 @@ class A2CAgent:
                 break
         
         self.state = state
-        action, log_prob, entropy, value = self.policy(state)
-#        value = self.value(state)
+#        _, _, _, next_value = self.policy(state)
+        next_value = self.value(state)
         
-#        advantages = []
         returns = []
+        advantages = []
         
-#        adv = torch.FloatTensor(np.zeros((num_envs, 1)))
-        ret = value.detach()
+        ret = next_value.detach()
+        adv = torch.zeros((num_envs, 1))
         for i in reversed(range(len(rewards))):
-            # returns = R + γV(s)
-            ret = rewards[i] + config.gamma * ret * masks[i]
-            returns.insert(0, ret.detach())
+            # R + γV(s')
+            ret = rewards[i] + (config.gamma ** i) * ret * masks[i]
+            returns.insert(0, ret)
             
-            # advantages = R + γV(s') - V(s)
-#            adv = ret - values[i].detach()
-#            advantages.insert(0, adv.detach())
-        
+            # R + γV(s') - V(s)
+            adv = ret - values[i].detach()
+            advantages.insert(0, adv)
+            
         log_probs = torch.cat(log_probs)
         values = torch.cat(values)
         returns = torch.cat(returns)
-#        advantages = torch.cat(advantages)
+        advantages = torch.cat(advantages)
         entropies = torch.cat(entropies)
         
-        # advantages = R + γV(s') - V(s)
-        advantages = (returns - values)
-        
-        policy_loss = (-log_probs * advantages.detach()).mean()
-        value_loss = advantages.pow(2).mean()
+        policy_loss = (-log_probs * advantages).mean()
+        value_loss = nn.MSELoss()(returns, values)
         entropy_loss = entropies.mean()
         
-#        policy_loss = -(log_probs * advantages).sum()
+#        policy_loss = (-log_probs * advantages).sum()
 #        value_loss = (returns - values).pow(2).sum()
 #        entropy_loss = entropies.sum()
         
-        loss = (policy_loss - config.ent_weight * entropy_loss + config.val_loss_weight * value_loss)
-#        loss = (policy_loss - config.ent_weight * entropy_loss)
+#        loss = (policy_loss - \
+#                config.ent_weight * entropy_loss + \
+#                config.val_loss_weight * value_loss)
+        loss = (policy_loss - config.ent_weight * entropy_loss)
         
-        self.optim.zero_grad()
-        loss.backward()
-        nn.utils.clip_grad_norm_(self.policy.parameters(), config.grad_clip)
-        self.optim.step()
-        
-#        self.optim_policy.zero_grad()
+#        self.optim.zero_grad()
 #        loss.backward()
 #        nn.utils.clip_grad_norm_(self.policy.parameters(), config.grad_clip)
-#        self.optim_policy.step()
-#        
-#        self.optim_value.zero_grad()
-#        value_loss.backward()
-#        nn.utils.clip_grad_norm_(self.value.parameters(), config.grad_clip)
-#        self.optim_value.step()
+#        self.optim.step()
+        
+        self.optim_policy.zero_grad()
+        self.optim_value.zero_grad()
+        
+        loss.backward()
+        nn.utils.clip_grad_norm_(self.policy.parameters(), config.grad_clip)
+        
+        value_loss.backward()
+        nn.utils.clip_grad_norm_(self.value.parameters(), config.grad_clip)
+        
+        self.optim_policy.step()
+        self.optim_value.step()
         
         return done.any(), policy_loss, value_loss, entropy_loss, loss
     
@@ -259,7 +270,7 @@ class A2CAgent:
             if i_episode % config.log_every == 0:
                 score = self.eval_episode()
                 
-                print('Episode {}\tEval score: {:.2f}\tPolicy loss: {:.2f}\tValue loss: {:.2f}\tEntropy loss: {:.2f}\tTotal loss: {:.2f}'\
+                print('Episode {}\tEval score: {:.2f}\tPolicy loss: {:.2f}\tValue loss: {:.2f}\tEntropy loss: {:.2f}\tLoss: {:.2f}'\
                       .format(i_episode, 
                               score, 
                               policy_loss, 
@@ -277,34 +288,44 @@ class A2CAgent:
         env = self.config.eval_env
         state = env.reset()
         
-        score = 0
+        total_score = 0
         
         while True:
             action = self.act([state])
             action = action.item()
             state, reward, done, _ = env.step(action)
             
-            score += reward
+            total_score += reward
             
             if done:
                 break
         
-        return score
+        return total_score
     
-    def run_episode(self):
+    def run_episode(self, debug=True):
         env = self.config.envs
         state = env.reset()
         
+        total_score = 0
+        
         env.render()
-            
+        
         while True:
             action = self.act([state])
             action = action.item()
             state, reward, done, _ = env.step(action)
+            
+            total_score += reward
+            
+            if debug:
+                print('Reward: {:.2f}'.format(reward))
             
             env.render()
             
             if done:
                 break
         
+        if debug:
+            print('Total reward: {:.2f}'.format(total_score))
+                
         env.close()
--- test.py
+++ test.py
@@ -14,11 +14,11 @@ config.env_name = 'LunarLander-v2' # RocketLander-v0 | LunarLander-v2 | Mountain
 config.envs = gym.make(config.env_name)
 config.state_dim = config.envs.observation_space.shape[0]
 config.action_dim = config.envs.action_space.n
-config.hidden_units = (64, 64)
-config.activ = torch.tanh
+config.hidden_units = (128,)
+config.activ = F.tanh
 
 agent = A2CAgent(config)
 
 agent.policy.load_state_dict(torch.load('policy_weights.pth'))
 
-agent.run_episode()
+agent.run_episode(debug=True)
